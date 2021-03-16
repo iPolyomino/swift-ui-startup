0475137b03d42ae51093e637480e7efed8895a44
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-03-16 10:08:32 +0900

    Initial Commit

---
+++ swift-ui-startup/swift_ui_startupApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_startupApp.swift
+//  swift-ui-startup
+//
+//  Created by 萩倉丈 on 2021/03/16.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_startupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
